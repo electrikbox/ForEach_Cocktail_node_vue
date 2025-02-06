a273fe82b9f9334afa53644b379ecccf9451fb03
electrikbox <electrikbox@icloud.com>
2025-02-06 13:58:51 +0100

    add transition pages

--- frontend/front/src/App.vue
+++ frontend/front/src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <Header></Header>
   <div class="d-flex justify-content-center">
-    <RouterView></RouterView>
+    <transition name="zoom" mode="out-in">
+      <RouterView></RouterView>
+    </transition>
   </div>
 </template>
 
 <script setup>
 import Header from './components/Header.vue';
 </script>
+
+<style>
+.zoom-enter-active, .zoom-leave-active {
+  transition: all 0.3s ease;
+}
+.zoom-enter-from, .zoom-leave-to {
+  transform: scale(0.95);
+  opacity: 0;
+}
+</style>
