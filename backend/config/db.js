0e072600db2703e4b7e0f86220b5148570e2e301
electrikbox <electrikbox@icloud.com>
2025-02-06 16:51:28 +0100

    use pool

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,15 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-// Create connection
-export const db = mysql.createConnection({
+// Création du pool de connexions
+export const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
