65b913517f90db83a14ddbe61a546847e05ed01f
electrikbox <electrikbox@icloud.com>
2025-02-06 14:00:45 +0100

    refact

--- frontend/front/src/components/Modal.vue
+++ frontend/front/src/components/Modal.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="modal fade show d-block" tabindex="-1" role="dialog" @click.self="close">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <slot name="header"></slot>
-          <button type="button" class="btn-close" aria-label="Close" @click="close"></button>
-        </div>
-        <div class="modal-body">
-          <slot name="body"></slot>
-          <slot name="verre"></slot>
-          <slot name="garniture"></slot>
-          <slot name="categories"></slot>
-          <slot name="image"></slot>
+    <div class="modal fade show d-block" tabindex="-1" role="dialog" @click.self="close">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <slot name="header"></slot>
+            <button type="button" class="btn-close" aria-label="Close" @click="close"></button>
+          </div>
+          <div class="modal-body">
+            <slot name="body"></slot>
+            <slot name="verre"></slot>
+            <slot name="garniture"></slot>
+            <slot name="categories"></slot>
+            <slot name="image"></slot>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 </template>
 
 
@@ -30,5 +30,6 @@ const close = () => emit('close');
 <style scoped>
 .modal.fade.show.d-block {
   background: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(5px);
 }
 </style>
