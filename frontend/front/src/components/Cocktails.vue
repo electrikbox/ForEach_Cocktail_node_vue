c7556bc5ac3ee2e3de6b681df4a3f12c45575e87
electrikbox <electrikbox@icloud.com>
2025-02-06 13:59:30 +0100

    add ingredients

--- frontend/front/src/components/Cocktails.vue
+++ frontend/front/src/components/Cocktails.vue
@@ -16,18 +16,28 @@
       <template v-slot:header>
         <h5>{{ selectedCocktail.nom }}</h5>
       </template>
+
       <template v-slot:body>
         <div class="d-flex justify-content-center">
           <img :src="`/images/${selectedCocktail.id}.jpg`" class="img-fluid mb-3" alt="Cocktail Image" @error="handleError">
         </div>
+
         <p>{{ selectedCocktail.description }}</p>
         <p class="m-0 p-0"><strong>Verre: </strong>{{ selectedCocktail.verre }}</p>
         <p class="m-0 p-0"><strong>Garniture:</strong> {{ selectedCocktail.garniture }}</p>
+        
+        <hr>
+        
+        <p class="m-0 p-0"><strong>Ingredients:</strong></p>
+        <ul>
+          <li v-for="ingredient in selectedCocktail.ingredients">{{ ingredient.nom }}: <strong>{{ ingredient.quantite }}</strong></li>
+        </ul>
+
+        <hr>
+
         <div class="d-flex flex-wrap">
           <p class="m-0 p-0"><strong>Categories:</strong> </p>
-          <span v-for="cat in selectedCocktail.categories" :key="cat.id" class="badge bg-dark-red m-1">
-            {{ cat.nom }}
-          </span>
+          <span v-for="cat in selectedCocktail.categories" :key="cat.id" class="badge bg-dark-red m-1">{{ cat.nom }}</span>
         </div>
       </template>
     </Modal>
@@ -40,7 +50,7 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useToast } from 'vue-toastification';
-import api from "../api.js";
+import api from "../composables/api";
 import Modal from "./Modal.vue";
 import CocktailCard from "./CocktailCard.vue";
 
